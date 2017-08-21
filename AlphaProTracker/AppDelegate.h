e1140b51aa73a8ce3cef5c1eca2457a1ef264f97
AgaramDevelopment <agaramdevelopment@gmail.com>
2017-08-21 19:14:33 +0530

    Initial Commit

---
+++ AlphaProTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlphaProTracker
+//
+//  Created by Mac on 21/08/17.
+//  Copyright © 2017 agaraminfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlphaProTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphaProTracker
+//
+//  Created by Mac on 21/08/17.
+//  Copyright © 2017 agaraminfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
