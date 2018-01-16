67c44bf83291ff29e59b02bd4aed9468f72b519f
Apple <apple@Apples-MacBook-Pro.local>
2018-01-16 11:50:58 +0530

    M file commit

---
+++ AlphaProTracker/Subview/HomeVCCell/HomeVCCell.m
@@ -0,0 +1,21 @@
+//
+//  HomeVCCell.m
+//  AlphaProTracker
+//
+//  Created by Lexicon on 23/08/17.
+//  Copyright © 2017 agaraminfotech. All rights reserved.
+//
+
+#import "HomeVCCell.h"
+
+@interface HomeVCCell ()
+
+@end
+
+@implementation HomeVCCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+}
+@end
