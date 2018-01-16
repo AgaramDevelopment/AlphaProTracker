652a1fe832d08a9650bbbcacad3fa5d606b44144
AgaramDevelopment <agaramdevelopment@gmail.com>
2018-01-16 19:23:35 +0530

    multi injury commit

---
+++ AlphaProTracker/Controllers/MultiInjuryVC/MultiInjuryVC.h
@@ -0,0 +1,13 @@
+//
+//  MultiInjuryVC.h
+//  AlphaProTracker
+//
+//  Created by Mac on 16/01/18.
+//  Copyright © 2018 agaraminfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultiInjuryVC : UIViewController
+
+@end
